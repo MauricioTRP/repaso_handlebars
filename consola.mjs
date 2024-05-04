c2b5133fc2e2d75a101b82f70e2da49718c755d8
Mauricio Fuentes <mauriciofb@gmail.com>
2024-05-04 12:53:48 -0400

    Uso de librerías

---
+++ consola.mjs
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+console.log(chalk.blue.bgRed.bold("Un mensaje en azul"))
+
+console.log(new Date())
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // 1.- Importamos librerías
 const express = require('express')
 const exphbs = require('express-handlebars')
+const { v4: uuidv4 } = require('uuid')
+const moment = require('moment')
+const _ = require('lodash')
+const axios = require('axios')
 
 // 2.- Instancia del servidor
 const app = express()
@@ -19,6 +23,7 @@ app.use("/popper", express.static(__dirname + "/node_modules/@popperjs/core/dist
 
 // 5.- Definimos rutas
 app.get("/admin", (req, res) => {
+  console.log(uuidv4())
   res.render("admin", {
     layout: 'con_footer',
     configuraciones: ["dato1", "dato2", "dato3"]
@@ -26,9 +31,41 @@ app.get("/admin", (req, res) => {
 })
 
 app.get("/productos", (req, res) => {
+  // 30/05/1960
+  // 30 de abril
+
+  console.log(moment().format("DD/MM/YYYY"))
+  console.log(moment().format("[Hoy es] DD [de] MMMM"))
   res.render("productos")
 })
 
 app.get("/usuarios", (req, res) => {
   res.render("usuarios")
+})
+
+app.get("/futuro", (req, res) => {
+  const futuro = moment().add(10000, "days").format("[Estamos en el año] YYYY [y han pasado] YY [años], [hoy es:] DD/MMMM")
+
+  res.send(futuro)
+})
+
+app.get("/separados", (req, res) => {
+  const numeros = [1, 2, 3, 4, 5]
+
+  console.log(_.partition(numeros, n => n % 2))
+  res.send("Revisa la consola")
+})
+
+app.get("/rickymorty", (req, res) => {
+  const url = "https://rickandmortyapi.com/api/character/1"
+
+  axios
+    .get(url)
+    .then(({ data }) => {
+      res.send(data)
+    })
+    .catch(err => {
+      console.log(err)
+      res.send("Problemas al conseguir la data, intentalo de nuevo")
+    })
 })
